0820bbe172ecd9a8b687e4919584335bcd006acc
ultradr3mer <dulz@hotmail.de>
2023-11-22 17:59:24 +0100

    Changes

--- Jacobi.Vst.Samples.MidiNoteSampler/Generator.cs
+++ Jacobi.Vst.Samples.MidiNoteSampler/Generator.cs
@@ -8,6 +8,8 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using VstNetAudioPlugin.Dsp;
 
 namespace Jacobi.Vst.Samples.MidiNoteSampler
 {
@@ -22,22 +24,103 @@ namespace Jacobi.Vst.Samples.MidiNoteSampler
     public long sampleIndex = 0;
 
     private HashSet<byte> keys = new HashSet<byte>();
+    private Dictionary<byte, float> actuationDictionary = new Dictionary<byte, float>();
 
-    public bool IsPlaying => this.keys.Any();
+    public bool IsPlaying => keys.Any() || this.actuationDictionary.Any();
 
     internal void Generate(float sampleRate, VstAudioBuffer[] outChannels)
     {
-      foreach (var channel in outChannels)
-      {
-        for (int i = 0; i < channel.SampleCount; i++)
-        {
-          double time = (sampleIndex + i) / sampleRate * 60.0;
 
-          channel[i] = this.keys.Sum(x => (float)Math.Sin(time * noteFrequencies[x] / Pi2));
+      int length = outChannels[0].SampleCount;
+      for (int i = 0; i < length; i++)
+      {
+        foreach (var key in keys)
+        {
+          if (!actuationDictionary.TryGetValue(key, out float keyActuation))
+          {
+            keyActuation = 0;
+          }
+
+          if (keyActuation < 1)
+          {
+            if (DelayParameters.Attack == 0)
+            {
+              actuationDictionary[key] = 1;
+            }
+            else
+            {
+              actuationDictionary[key] = (float)Math.Min(keyActuation + 1.0 / DelayParameters.Attack / sampleRate, 1.0);
+            }
+          }
+        }
+
+        foreach (var item in actuationDictionary)
+        {
+          if (keys.Contains(item.Key))
+          {
+            continue;
+          }
+
+          float keyActuation;
+          if (DelayParameters.Release == 0)
+          {
+            keyActuation = 0;
+          }
+          else
+          {
+            keyActuation = (float)Math.Max(item.Value - 1.0 / DelayParameters.Release / sampleRate, 0.0);
+          }
+
+          if (keyActuation <= 0)
+          {
+            actuationDictionary.Remove(item.Key);
+          }
+          else
+          {
+            actuationDictionary[item.Key] = keyActuation;
+          }
+        }
+
+        double time = (sampleIndex + i) / sampleRate;
+        foreach (var channel in outChannels)
+        {
+          channel[i] = (float)this.actuationDictionary.Sum(actuation => 
+            actuation.Value * (DelayParameters.FmMod ? DelayParameters.Notes.Aggregate<int, double>(1.0, (a, o) => a * 1.5 * Wave(DelayParameters.Saw, time * noteFrequencies[actuation.Key] * o / 12.0)) :
+                                                       DelayParameters.Notes.Sum(o => Wave(DelayParameters.Saw, time * noteFrequencies[actuation.Key] * o / 12.0))));
         }
       }
 
       sampleIndex += outChannels.FirstOrDefault()?.SampleCount ?? 0;
+    }
+
+    static double Wave(double saw, double t)
+    {
+      t = t % 1;
+      double segment13_length = (1.0 - saw) / 4.0;
+      double segment2_length = (1.0 + saw) / 2.0;
+
+      if (t < segment13_length)
+      {
+        t = t / (1 - saw);
+      }
+      else if (t < (segment13_length + segment2_length))
+      {
+        t = (t - segment13_length) / (1.0 + saw) + (1.0 / 4.0);
+      }
+      else
+      {
+        t = 1 - (1 - t) / (1 - saw);
+      }
+
+      double sin_wave = Math.Sin(2 * Math.PI * t);
+
+      double saw_wave = t < (1.0 / 4.0) ? t * 4.0 :
+        t < (3.0 / 4.0) ? 1 - (t - (1.0 / 4.0)) * 4.0 :
+        (-4.0 + (4.0 * t));
+
+      double combined = (1 - saw) * sin_wave + saw * saw_wave;
+
+      return Math.Pow(Math.Abs(combined),0.5)*Math.Sign(combined);
     }
 
     internal void ProcessNoteOffEvent(byte v)
--- Jacobi.Vst.Samples.MidiNoteSampler/UI/PluginEditorView.Designer.cs
+++ Jacobi.Vst.Samples.MidiNoteSampler/UI/PluginEditorView.Designer.cs
@@ -1,223 +1,131 @@
 ﻿namespace VstNetAudioPlugin.UI
 {
-    partial class PluginEditorView
+  partial class PluginEditorView
+  {
+    /// <summary> 
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary> 
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
     {
-        /// <summary> 
-        /// Required designer variable.
-        /// </summary>
-        private System.ComponentModel.IContainer components = null;
-
-        /// <summary> 
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing && (components != null))
-            {
-                components.Dispose();
-            }
-            base.Dispose(disposing);
-        }
-
-        #region Component Designer generated code
-
-        /// <summary> 
-        /// Required method for Designer support - do not modify 
-        /// the contents of this method with the code editor.
-        /// </summary>
-        private void InitializeComponent()
-        {
-            System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
-            this.label1 = new System.Windows.Forms.Label();
-            this.trackBar1 = new System.Windows.Forms.TrackBar();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
-            this.label8 = new System.Windows.Forms.Label();
-            this.trackBar2 = new System.Windows.Forms.TrackBar();
-            this.trackBar3 = new System.Windows.Forms.TrackBar();
-            this.trackBar4 = new System.Windows.Forms.TrackBar();
-            tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            tableLayoutPanel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.trackBar4)).BeginInit();
-            this.SuspendLayout();
-            // 
-            // tableLayoutPanel1
-            // 
-            tableLayoutPanel1.ColumnCount = 3;
-            tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 32.14286F));
-            tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 67.85714F));
-            tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 62F));
-            tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
-            tableLayoutPanel1.Controls.Add(this.trackBar1, 1, 0);
-            tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
-            tableLayoutPanel1.Controls.Add(this.label3, 0, 2);
-            tableLayoutPanel1.Controls.Add(this.label4, 0, 3);
-            tableLayoutPanel1.Controls.Add(this.label5, 2, 0);
-            tableLayoutPanel1.Controls.Add(this.label6, 2, 1);
-            tableLayoutPanel1.Controls.Add(this.label7, 2, 2);
-            tableLayoutPanel1.Controls.Add(this.label8, 2, 3);
-            tableLayoutPanel1.Controls.Add(this.trackBar2, 1, 1);
-            tableLayoutPanel1.Controls.Add(this.trackBar3, 1, 2);
-            tableLayoutPanel1.Controls.Add(this.trackBar4, 1, 3);
-            tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
-            tableLayoutPanel1.Name = "tableLayoutPanel1";
-            tableLayoutPanel1.RowCount = 4;
-            tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
-            tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
-            tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
-            tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
-            tableLayoutPanel1.Size = new System.Drawing.Size(334, 248);
-            tableLayoutPanel1.TabIndex = 2;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(3, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(46, 17);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
-            // 
-            // trackBar1
-            // 
-            this.trackBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
-            this.trackBar1.Location = new System.Drawing.Point(90, 3);
-            this.trackBar1.Name = "trackBar1";
-            this.trackBar1.Size = new System.Drawing.Size(178, 56);
-            this.trackBar1.TabIndex = 1;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(3, 62);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(46, 17);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "label2";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(3, 124);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(46, 17);
-            this.label3.TabIndex = 6;
-            this.label3.Text = "label3";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(3, 186);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(46, 17);
-            this.label4.TabIndex = 9;
-            this.label4.Text = "label4";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(274, 0);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(46, 17);
-            this.label5.TabIndex = 2;
-            this.label5.Text = "label5";
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(274, 62);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(46, 17);
-            this.label6.TabIndex = 5;
-            this.label6.Text = "label6";
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(274, 124);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(46, 17);
-            this.label7.TabIndex = 8;
-            this.label7.Text = "label7";
-            // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(274, 186);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(46, 17);
-            this.label8.TabIndex = 11;
-            this.label8.Text = "label8";
-            // 
-            // trackBar2
-            // 
-            this.trackBar2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
-            this.trackBar2.Location = new System.Drawing.Point(90, 65);
-            this.trackBar2.Name = "trackBar2";
-            this.trackBar2.Size = new System.Drawing.Size(178, 56);
-            this.trackBar2.TabIndex = 4;
-            // 
-            // trackBar3
-            // 
-            this.trackBar3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
-            this.trackBar3.Location = new System.Drawing.Point(90, 127);
-            this.trackBar3.Name = "trackBar3";
-            this.trackBar3.Size = new System.Drawing.Size(178, 56);
-            this.trackBar3.TabIndex = 7;
-            // 
-            // trackBar4
-            // 
-            this.trackBar4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
-            this.trackBar4.Location = new System.Drawing.Point(90, 189);
-            this.trackBar4.Name = "trackBar4";
-            this.trackBar4.Size = new System.Drawing.Size(178, 56);
-            this.trackBar4.TabIndex = 10;
-            // 
-            // PluginEditorView
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(tableLayoutPanel1);
-            this.Margin = new System.Windows.Forms.Padding(4);
-            this.Name = "PluginEditorView";
-            this.Size = new System.Drawing.Size(334, 248);
-            tableLayoutPanel1.ResumeLayout(false);
-            tableLayoutPanel1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.trackBar4)).EndInit();
-            this.ResumeLayout(false);
-
-        }
-
-        #endregion
-
-        private System.Windows.Forms.TrackBar trackBar1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.TrackBar trackBar2;
-        private System.Windows.Forms.TrackBar trackBar3;
-        private System.Windows.Forms.TrackBar trackBar4;
-
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
     }
+
+    #region Component Designer generated code
+
+    /// <summary> 
+    /// Required method for Designer support - do not modify 
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+      trackBar1 = new System.Windows.Forms.TrackBar();
+      FmModulation = new System.Windows.Forms.CheckBox();
+      attack = new System.Windows.Forms.TrackBar();
+      release = new System.Windows.Forms.TrackBar();
+      trackBar2 = new System.Windows.Forms.TrackBar();
+      ((System.ComponentModel.ISupportInitialize)trackBar1).BeginInit();
+      ((System.ComponentModel.ISupportInitialize)attack).BeginInit();
+      ((System.ComponentModel.ISupportInitialize)release).BeginInit();
+      ((System.ComponentModel.ISupportInitialize)trackBar2).BeginInit();
+      SuspendLayout();
+      // 
+      // checkedListBox1
+      // 
+      checkedListBox1.CheckOnClick = true;
+      checkedListBox1.FormattingEnabled = true;
+      checkedListBox1.Location = new System.Drawing.Point(0, 0);
+      checkedListBox1.Name = "checkedListBox1";
+      checkedListBox1.Size = new System.Drawing.Size(180, 220);
+      checkedListBox1.TabIndex = 0;
+      checkedListBox1.ItemCheck += checkedListBox1_ItemCheck;
+      checkedListBox1.SelectedIndexChanged += checkedListBox1_SelectedIndexChanged;
+      // 
+      // trackBar1
+      // 
+      trackBar1.Location = new System.Drawing.Point(186, 3);
+      trackBar1.Maximum = 100;
+      trackBar1.Name = "trackBar1";
+      trackBar1.Size = new System.Drawing.Size(161, 45);
+      trackBar1.TabIndex = 1;
+      trackBar1.ValueChanged += trackBar1_ValueChanged;
+      // 
+      // FmModulation
+      // 
+      FmModulation.AutoSize = true;
+      FmModulation.Location = new System.Drawing.Point(186, 43);
+      FmModulation.Name = "FmModulation";
+      FmModulation.Size = new System.Drawing.Size(105, 19);
+      FmModulation.TabIndex = 2;
+      FmModulation.Text = "FmModulation";
+      FmModulation.UseVisualStyleBackColor = true;
+      FmModulation.CheckedChanged += FmModulation_CheckedChanged;
+      // 
+      // attack
+      // 
+      attack.Location = new System.Drawing.Point(186, 119);
+      attack.Maximum = 100;
+      attack.Name = "attack";
+      attack.Size = new System.Drawing.Size(161, 45);
+      attack.TabIndex = 3;
+      attack.ValueChanged += attack_ValueChanged;
+      // 
+      // release
+      // 
+      release.Location = new System.Drawing.Point(186, 170);
+      release.Maximum = 100;
+      release.Name = "release";
+      release.Size = new System.Drawing.Size(161, 45);
+      release.TabIndex = 4;
+      release.ValueChanged += release_ValueChanged;
+      // 
+      // trackBar2
+      // 
+      trackBar2.Location = new System.Drawing.Point(186, 68);
+      trackBar2.Maximum = 100;
+      trackBar2.Name = "trackBar2";
+      trackBar2.Size = new System.Drawing.Size(161, 45);
+      trackBar2.TabIndex = 5;
+      trackBar2.Value = 50;
+      // 
+      // PluginEditorView
+      // 
+      AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+      AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      Controls.Add(trackBar2);
+      Controls.Add(release);
+      Controls.Add(attack);
+      Controls.Add(FmModulation);
+      Controls.Add(trackBar1);
+      Controls.Add(checkedListBox1);
+      Margin = new System.Windows.Forms.Padding(4);
+      Name = "PluginEditorView";
+      Size = new System.Drawing.Size(351, 232);
+      ((System.ComponentModel.ISupportInitialize)trackBar1).EndInit();
+      ((System.ComponentModel.ISupportInitialize)attack).EndInit();
+      ((System.ComponentModel.ISupportInitialize)release).EndInit();
+      ((System.ComponentModel.ISupportInitialize)trackBar2).EndInit();
+      ResumeLayout(false);
+      PerformLayout();
+    }
+
+    #endregion
+
+    private System.Windows.Forms.CheckedListBox checkedListBox1;
+    private System.Windows.Forms.TrackBar trackBar1;
+    private System.Windows.Forms.CheckBox FmModulation;
+    private System.Windows.Forms.TrackBar attack;
+    private System.Windows.Forms.TrackBar release;
+    private System.Windows.Forms.TrackBar trackBar2;
+  }
 }
