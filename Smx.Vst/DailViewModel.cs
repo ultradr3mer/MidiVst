a4ed1119b6ce05ff5d4a7d02023496f1f45f7a40
Clara Schulz-Theißen <dulz@hotmail.de>
2023-12-25 20:37:33 +0100

    Rotary Dail control

---
+++ Smx.Vst/DailViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+
+namespace Smx.Vst
+{
+  internal class DailViewModel : INotifyPropertyChanged
+  {
+    public DailViewModel()
+    {
+    }
+
+    public bool IsPressed { get; set; }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+  }
+}
--- Smx.Vst/UI/RotaryDail.xaml.cs
+++ Smx.Vst/UI/RotaryDail.xaml.cs
@@ -1,16 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Smx.Vst.Util;
+using System;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Smx.Vst.UI
@@ -20,14 +13,45 @@ namespace Smx.Vst.UI
   /// </summary>
   public partial class RotaryDail : UserControl
   {
-    private bool isPressed;
-    private Point mouseStart;
+    private double fill = 0.6;
     private double fillStart;
-    double fill = 0.6;
+    private Point mouseStart;
+    private DailViewModel viewModel;
 
     public RotaryDail()
     {
       InitializeComponent();
+      this.viewModel = new DailViewModel();
+      this.DataContext = this.viewModel;
+
+      this.MouseDown += Button_MouseDown;
+      this.MouseUp += Button_MouseUp;
+      this.MouseMove += Button_MouseMove;
+    }
+
+
+    private void Button_MouseDown(object sender, MouseButtonEventArgs e)
+    {
+      this.viewModel.IsPressed = true;
+      this.mouseStart = Mouse.GetPosition(this);
+      this.fillStart = this.fill;
+    }
+
+    private void Button_MouseMove(object sender, MouseEventArgs e)
+    {
+      if (this.viewModel.IsPressed)
+      {
+        double mouseDelta = Mouse.GetPosition(this).Y - this.mouseStart.Y;
+        this.fill = Math.Clamp(fillStart - mouseDelta / 100.0, 0.0, 1.0);
+        this.UpdateGeometry();
+      }
+    }
+
+    private void Button_MouseUp(object sender, MouseButtonEventArgs e)
+    {
+      this.viewModel.IsPressed = false;
+      this.mouseStart = new Point(0, 0);
+      this.fillStart = 0;
     }
 
     private void Path_SizeChanged(object sender, SizeChangedEventArgs e)
@@ -37,7 +61,7 @@ namespace Smx.Vst.UI
 
     private void UpdateGeometry()
     {
-      double rad = Math.PI * 2.0 * fill;
+      double rad = Math.PI * 2.0 * Math.Clamp(fill, 0.001,0.999);
 
       double radius = OuterEllipse.ActualWidth / 2.0;
 
@@ -49,10 +73,10 @@ namespace Smx.Vst.UI
       {
         ctx.BeginFigure(new Point(0, 0), true /* is filled */, true /* is closed */);
         ctx.LineTo(new Point(0, radius), true /* is stroked */, false /* is smooth join */);
-        ctx.ArcTo(new Point(Math.Sin(rad) * radius, -Math.Cos(rad) * radius),
+        ctx.ArcTo(new Point(-Math.Sin(rad) * radius, Math.Cos(rad) * radius),
           new Size(radius, radius),
           rotationAngle: 0,
-          isLargeArc: false,
+          isLargeArc: fill > 0.5f,
           sweepDirection: SweepDirection.Clockwise,
           isStroked: true,
           isSmoothJoin: false
@@ -62,43 +86,9 @@ namespace Smx.Vst.UI
       myPath.Data = geometry;
     }
 
-    private void UserControl_Initialized(object sender, EventArgs e)
-    {
-      
-    }
-
-    private void MainGrid_MouseDown(object sender, MouseButtonEventArgs e)
+    private void UserControl_MouseUp(object sender, MouseButtonEventArgs e)
     {
 
-    }
-
-    private void Button_MouseDown(object sender, MouseButtonEventArgs e)
-    {
-      this.isPressed = true;
-      this.mouseStart = Mouse.GetPosition(this);
-      this.fillStart = this.fill;
-    }
-
-    private void Button_MouseUp(object sender, MouseButtonEventArgs e)
-    {
-      this.isPressed = false;
-      this.mouseStart = new Point(0, 0);
-      this.fillStart = 0;
-    }
-
-    private void Button_MouseMove(object sender, MouseEventArgs e)
-    {
-      if (this.isPressed)
-      {
-        double mouseDelta = Mouse.GetPosition(this).Y - this.mouseStart.Y;
-        this.fill = fillStart - mouseDelta / 100.0;
-        this.UpdateGeometry();
-      }
-    }
-
-    private void Button_Click(object sender, RoutedEventArgs e)
-    {
-  
     }
   }
 }
